9d61787d523208f3afd5f521236d7e2ea461ef6b
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-10-14 19:21:26 -0300

    Questão 3

---
+++ principal3.java
@@ -0,0 +1,35 @@
+package Street_fighter;
+
+public class principal3 {
+
+	public static void main(String[] args) {
+		
+		lutador ryu = new lutador("ryu", 100, 10);
+		lutador Bison = new lutador("Bison", 100, 12);
+		
+		ryu.aplicarGolpe("Bison");
+		ryu.aplicarGolpe("Bison");
+		ryu.aplicarGolpe("Bison");
+		
+		System.out.println ("energia do bison: " +Bison.energia);
+		
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+		Bison.aplicarGolpe("Ryu");
+
+		System.out.println ("energia Ryu: " +ryu.energia);
+		
+		
+		
+		
+		
+
+	}
+
+	
+}
