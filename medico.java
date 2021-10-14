d03f2e7ab81b783bfc6f1413f2ec879b345829bf
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-10-14 14:34:46 -0300

    Questão 2

---
+++ medico.java
@@ -0,0 +1,22 @@
+package CLINICAMEDICA;
+
+public class medico {
+	int codigo;
+	String nome;
+	String sexo;
+	String especialidade;
+	String endereço;
+	
+	public medico(int codigo, String nome, String sexo, String espeialidade, String endereço) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.especialidade = espeialidade;
+		this.endereço = endereço;
+	}
+	
+	
+	
+	
+
+}
