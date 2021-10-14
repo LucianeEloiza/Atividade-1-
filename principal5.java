66fedf13fb0f2ac58ea1c824143312a70c9f48b5
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-10-14 19:18:22 -0300

    Questão 5

---
+++ principal5.java
@@ -0,0 +1,30 @@
+package SUAP;
+
+public class principal5 {
+
+	public static void main(String[] args) {
+		
+		Aluno A1 = new Aluno ("Luciane Eloiza", "098.608.450-59");
+		A1.nota1 = 9;
+		A1.nota2 = 8;
+		A1.nota3 = 9;
+		
+		A1.Calcularmedia();
+		
+		System.out.println("media será de: " + A1.Calcularmedia());
+		System.out.println("modificando 2° nota..." );
+		
+		A1.nota1 = 9;
+		A1.nota2 = 10;
+		A1.nota3 = 9;
+		
+		A1.Calcularmedia();
+		
+		System.out.println("media será de: " + A1.Calcularmedia());
+		
+        
+        
+         
+	}
+
+}
