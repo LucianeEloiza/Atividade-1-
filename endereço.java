a0a488177e57fea0a0da99847e425377a14544aa
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-10-14 14:35:57 -0300

    QUESTÃO 2

---
+++ endereço.java
@@ -0,0 +1,33 @@
+package CLINICAMEDICA;
+
+public class endereço {
+
+ String rua;
+ int numero;
+ String complemento;
+ String bairro;
+ String cidade;
+ String uf;
+ int cep;
+
+ public endereço(String rua, int numero, String complemento, String bairro, String cidade, String uf, int cep) {
+	this.rua = rua;
+	this.numero = numero;
+	this.complemento = complemento;
+	this.bairro = bairro;
+	this.cidade = cidade;
+	this.uf = uf;
+	this.cep = cep;
+}
+
+
+
+}
+ 
+ 
+ 
+
+
+
+ 
+ 
